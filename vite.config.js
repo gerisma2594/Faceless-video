b0e752dad70a03a774d1881a853a70d79ad540fc
gerisma2594 <gerisma2594@gmail.com>
2026-06-25 17:53:03 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/content-studio/',
+  base: '/Faceless-video/',
   build: { outDir: 'dist' },
   define: { 'process.env': {} }
 })
